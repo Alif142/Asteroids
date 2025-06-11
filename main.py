41cba40218adfddf1ef9d6d10088e548ad9980b9
Alif142 <mizan.alif190@gmail.com>
2025-06-11 17:17:50 +0600

    CH-2 L7

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def main():
 
         screen.fill("black")
         player_instance.draw(screen)
+        player_instance.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(144) / 1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -20,3 +20,29 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PlAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotation -= PlAYER_TURN_SPEED * dt
+
+        if keys[pygame.K_d]:
+            self.rotation += PlAYER_TURN_SPEED * dt
+
+        if keys[pygame.K_w]:
+            self.move_Forward(dt)
+
+        if keys[pygame.K_s]:
+            self.move_Back(dt)
+
+    def move_Forward(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def move_Back(self, dt):
+        backward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position -= backward * PLAYER_SPEED * dt
