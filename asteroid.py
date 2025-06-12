357630b66d3217e0399b7393bc9f852c934097f1
Alif142 <mizan.alif190@gmail.com>
2025-06-12 10:07:45 +0600

    CH-3 L-7

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import *
+import random
 
 
 class Asteroid(CircleShape):
@@ -13,3 +15,14 @@ class Asteroid(CircleShape):
 
     def split(self):
         self.kill()
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return "This was a small asteroid"
+        else:
+            random_angle = int(random.uniform(0.2, 0.5) * 100)
+            a1 = Asteroid(self.position.x, self.position.y, self.radius)
+            a2 = Asteroid(self.position.x, self.position.y, self.radius)
+            a1.radius = self.radius - ASTEROID_MIN_RADIUS
+            a2.radius = self.radius - ASTEROID_MIN_RADIUS
+            a1.velocity = self.velocity = self.velocity.rotate(random_angle)
+            a2.velocity = self.velocity = self.velocity.rotate(-random_angle)
+            return a1, a2
